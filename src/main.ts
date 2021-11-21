7418e746aa8fc1580027ece8b6cd8e96e64c4ce7
Nathachai Thongniran <inid3a@gmail.com>
2021-11-21 23:29:25 +0700

    feat(cors): enable CORS

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   // TODO: apply ValidationPipe
+  app.enableCors()
   await app.listen(3002)
 }
 bootstrap()
